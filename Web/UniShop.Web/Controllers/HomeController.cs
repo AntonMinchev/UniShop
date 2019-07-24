5b4429540029724773dfaec59759e18ab38b053c
AntonMinchev <anton.minchevv@gmail.com>
2019-07-24 19:25:44 +0300

    Add Layout,logo,login and register

--- Web/UniShop.Web/Controllers/HomeController.cs
+++ Web/UniShop.Web/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using UniShop.Web.Models;
 
 namespace UniShop.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public IActionResult Index()
         {
